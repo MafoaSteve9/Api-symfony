1f839b93eb0c3828ae63bb053564eff8198a909d
Steve Mafoa <steve_mafoa@icloud.com>
2025-05-06 17:31:35 +0200

    Mise en place de mon entité Product

---
+++ src/Entity/Product.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    #[Groups(['product:read'])]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['product:read'])]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    #[Groups(['product:read'])]
+    private ?float $price = null;
+
+    #[ORM\Column(length: 100)]
+    #[Groups(['product:read'])]
+    private ?string $category = null;
+
+    #[ORM\Column]
+    #[Groups(['product:read'])]
+    private ?bool $stock = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getCategory(): ?string
+    {
+        return $this->category;
+    }
+
+    public function setCategory(string $category): static
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    public function getStock(): ?bool
+    {
+        return $this->stock;
+    }
+
+    public function setStock(bool $stock): static
+    {
+        $this->stock = $stock;
+
+        return $this;
+    }
+}
