e6ac4a0881463a87f569135e05510f9f759f73ec
Steve Mafoa <steve_mafoa@icloud.com>
2025-05-13 10:35:29 +0200

    fix: Ajustement de mes ENDPOINTS

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,29 +2,60 @@
 
 namespace App\Controller;
 
-use App\Service\CreateProductService;
+
 use App\Service\ProductService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 
 final class ProductController extends AbstractController
 {
-    #[Route('api/products', name: 'app_product_list', methods: ['GET'])]
-    public function index(ProductService $productService): JsonResponse
-    {   
-        $json = $productService->getAllProductsAsJson();
-        return new JsonResponse($json, 200, [], true);
-    }
+    private ProductService $productService;
 
-    #[Route('api/products', name: 'app_product_create', methods: ['POST'])]
-    public function  create(CreateProductService  $createProductService): JsonResponse
+    public function __construct(ProductService $productService)
     {
-        $json = $createProductService->createProduct();
-        return new JsonResponse($json, 201, [], true);
+        $this->productService = $productService;
+    }
+
+    #[Route('/api/products', name: 'app_product_list', methods: ['GET'])]
+    public function index(): JsonResponse
+    {
+        $products = $this->productService->getAllProducts();
+        return $this->json($products, 200, [], ['groups' => 'product:read']);
+    }
+
+    #[Route('/api/products', name: 'app_product_create', methods: ['POST'])]
+    public function create(Request $request): JsonResponse
+    {
+        $result = $this->productService->createProduct($request->getContent());
+
+        if (is_array($result) && isset($result['errors'])) {
+        return $this->json(['errors' => $result['errors']], 400);
+    }
+
+        return $this->json($result, 201, [], ['groups' => 'product:read']);
     }
 
 
-    
+    #[Route('/api/products/{id}', name: 'app_product_update', methods: ['PUT'])]
+    public function update(int $id, Request $request): JsonResponse
+    {
+    $result = $this->productService->updateProduct($id, $request->getContent());
+
+    if (isset($result['errors'])) {
+        return new JsonResponse(['errors' => $result['errors']], $result['status']);
+    }
+
+    return new JsonResponse($result['data'], $result['status'], [], true);
+    }
+
+
+    #[Route('/api/products/{id}', name: 'app_product_delete', methods: ['DELETE'])]
+    public function delete(int $id): JsonResponse
+    {
+        $result = $this->productService->deleteProduct($id);
+        return new JsonResponse($result['data'], $result['status']);
+    }
 }
